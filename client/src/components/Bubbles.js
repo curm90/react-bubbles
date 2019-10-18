09abb85b2b522c53b2d5b1266cea54450c8d830a
Liam Sutton <devliam90@gmail.com>
2019-10-18 10:58:00 +0100

    add interval for updating bubbles

--- client/src/components/Bubbles.js
+++ client/src/components/Bubbles.js
@@ -4,12 +4,21 @@ import { Svg, Circle } from '@potion/element';
 
 const Bubbles = ({ colors }) => {
   const [bubbleData, setBubbleData] = useState([]);
+
   useEffect(() => {
-    const generateBubbleData = colors.map((_, i) => ({
-      value: Math.floor(Math.random() * (colors.length * 2)) + 1,
-      key: `${i + 1}`
-    }));
+    const generateBubbleData = () => {
+      return colors.map((_, i) => ({
+        value: Math.floor(Math.random() * (colors.length * 2)) + 1,
+        key: `${i + 1}`
+      }));
+    };
     setBubbleData(generateBubbleData);
+
+    const updateBubblesInterval = setInterval(() => {
+      setBubbleData(generateBubbleData);
+    }, 2500);
+
+    return () => clearInterval(updateBubblesInterval);
   }, [colors]);
 
   return (
